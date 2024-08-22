9b50473a773db290404e8de89532641d251523dd
milogit <pascalmillimono@gmail.com>
2024-08-22 14:43:07 -0400

    data ingestion stage added

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ml-mlf-prj
 Machine Learning Modele implementation and deployment using MlFlow
+
+## Workflows
+1. Update config.yaml
+2. Update schema.yaml (data columns will be describe here)
+3. Update params.yaml
+4. Update the entity
+5. Update the configuration manager in src config
+6. Update the components
+7. Update the pipeline
+8. Upadate the main.py
+9. Update the dvc.yaml / Update the app.py
--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from mlProject import logger
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to custom log")
+#logger.info("Welcome to custom log")
+
+STAGE_NAME = "Data Ingestion stage"
+try :
+    logger.info(f">>>>> stage {STAGE_NAME} starte <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
